fae266c0502451653271721fc0fd5c70df410ecd
BijanProgrammer <bijaneisapour@gmail.com>
2024-01-10 15:45:42 +0330

    [UPDATE] theme colors

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -73,7 +73,7 @@ const config: Config = {
             ],
         },
         footer: {
-            style: 'dark',
+            style: 'light',
             links: [
                 {
                     title: 'دسته‌بندی‌ها',
